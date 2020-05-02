85ee7b5ff82028ddd9f84dd3ec0c51a31f045d81
snehkothari <46020044+snehkothari@users.noreply.github.com>
2020-05-02 21:56:02 +0530

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -3,10 +3,16 @@ const router = express.Router();
 
 const BooksController = require('../controllers/books');
 
-
+//Gets entire details of the booking
 router.get('/',BooksController.books_get_all);
+
+//Adding a booking to the database
 router.post('/',BooksController.add_bookings);
+
+//Get a particular booking based on the bookingid
 router.get("/:bookId",BooksController.get_single_booking);
+
+//Delete a booking based on its bookingid
 router.delete("/:bookId",BooksController.delete_booking);
 
 module.exports = router;
