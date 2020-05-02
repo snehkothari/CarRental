f6c8a8c22b6549221a7a9bc672509af55682017b
snehkothari <46020044+snehkothari@users.noreply.github.com>
2020-05-02 22:00:24 +0530

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+//Defining the schema of the booking, which requires properties like the car id and the dates for booking the car.
+//Each booking is assigned a unique id which can be used to access bookings seperately.
 const bookSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	car:{ type:mongoose.Schema.Types.ObjectId, ref:'Car', required:true },
