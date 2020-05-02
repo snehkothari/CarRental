e988cc993007459308f1bd17c53ccdf3e18a3895
snehkothari <46020044+snehkothari@users.noreply.github.com>
2020-05-02 22:02:13 +0530

    Update car.js

--- models/car.js
+++ models/car.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+//Defining the schema of a car with car number,rent per day, model, seating space and bookings(if any).
+//Booking is optional and can be left empty or not mentioned when adding a new car whereas other properties are required.
 const carSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	vehicle_no: { type:String, required: true},
